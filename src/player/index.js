5e7e8c280d7b865d89846c283474ebb6d11e5a3f
Erwann Le Noac'h <erwann.lenoach@hotmail.fr>
2021-10-26 18:40:08 +0200

    General - MVP

--- src/player/index.js
+++ src/player/index.js
@@ -1,55 +1,79 @@
 import React, {useEffect, useState} from 'react';
-import TrackPlayer, { State,
-  Capability,
-  PitchAlgorithm,
-  RatingType,
-  Event,
-  RepeatMode} from 'react-native-track-player';
+import TrackPlayer from 'react-native-track-player';
 import {View, StyleSheet} from 'react-native';
 import {faPlay, faPause} from '@fortawesome/free-solid-svg-icons';
 import {FontAwesomeIcon} from '@fortawesome/react-native-fontawesome';
 
 const Player = () => {
   const [displayBtnPlay, setDisplayBtnPlay] = useState(true);
-  useEffect(() => {console.log(displayBtnPlay)})
+  useEffect(() => {
+    setUpTrackPlayer();
+    return () => TrackPlayer.destroy();
+  });
 
+  let track = {
+    url: 'https://radio2.pro-fhi.net/radio/9111/stream.mp3',
+    title: 'Hotel Radio',
+    artist: 'Hotel Radio',
+    album: 'Hotel Radio',
+    genre: 'Hip-Hop, Electro',
+    date: '2014-05-20T07:00:00+00:00',
+    artwork:
+      'https://hotelradioparis.com/wp-content/uploads/2021/03/LogoELE.png',
+    duration: 402, // Duration in seconds
+  };
 
-    var track = {
-      url: 'https://radio2.pro-fhi.net/radio/9111/stream.mp3', 
-      title: 'Hotel Radio',
-      artist: 'Hotel Radio',
-      album: 'Hotel Radio',
-      genre: 'Hip-Hop, Electro',
-      date: '2014-05-20T07:00:00+00:00', 
-      artwork:
-        'https://hotelradioparis.com/wp-content/uploads/2021/03/LogoELE.png',
-      duration: 402, // Duration in seconds
-    };
-    
-  const start = () => {
-    TrackPlayer.destroy()
-    setDisplayBtnPlay(true)
+  TrackPlayer.updateOptions({
+    stopWithApp: false,
+    capabilities: [TrackPlayer.CAPABILITY_PLAY, TrackPlayer.CAPABILITY_PAUSE],
+    compactCapabilities: [
+      TrackPlayer.CAPABILITY_PLAY,
+      TrackPlayer.CAPABILITY_PAUSE,
+    ],
+  });
+
+  const setUpTrackPlayer = async () => {
+    try {
+    await TrackPlayer.setupPlayer();
+      await  TrackPlayer.add([track]);
+      console.log('Tracks added');
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  const start =  () => {
     console.log("start")
-    TrackPlayer.add([track]);
-  
+     setDisplayBtnPlay(true);
     TrackPlayer.play();
-    setDisplayBtnPlay(false)
-    
+    setDisplayBtnPlay(false);
   };
 
   const stop = async () => {
     console.log("stop")
     TrackPlayer.stop();
-    TrackPlayer.destroy()
-   setDisplayBtnPlay(true)
-
+    TrackPlayer.destroy();
+    setDisplayBtnPlay(true);
   };
+
 
   return (
     <View style={styles.container}>
       <View style={styles.buttonDiv}>
-      <FontAwesomeIcon icon={faPlay} size={40} style={styles.Btn} onPress={start} display={displayBtnPlay ? 'flex' : 'none'} />
-      <FontAwesomeIcon icon={faPause} size={40} style={styles.Btn} onPress={stop} display={!displayBtnPlay ? 'flex' : 'none'} />
+        <FontAwesomeIcon
+          icon={faPlay}
+          size={40}
+          style={styles.Btn}
+          onPress={start}
+          display={displayBtnPlay ? 'flex' : 'none'}
+        />
+        <FontAwesomeIcon
+          icon={faPause}
+          size={40}
+          style={styles.Btn}
+          onPress={stop}
+          display={!displayBtnPlay ? 'flex' : 'none'}
+        />
       </View>
     </View>
   );
@@ -64,7 +88,7 @@ const styles = StyleSheet.create({
   Btn: {
     alignSelf: 'center',
     color: 'white',
-    position: 'absolute', 
+    position: 'absolute',
   },
   buttonDiv: {
     height: 100,
@@ -75,8 +99,7 @@ const styles = StyleSheet.create({
     borderRadius: 100,
     borderColor: 'white',
     justifyContent: 'center',
-    alignItems: 'center'
-
-  }
+    alignItems: 'center',
+  },
 });
 export default Player;
